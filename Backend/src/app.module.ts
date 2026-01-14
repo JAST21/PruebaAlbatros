5513b4d983ef208ebe0e4a6d1bee27d51b4e35fa
JALLMAR ANDRES SAMUELS TORRES <jallmar.samuels@ucr.ac.cr>
2026-01-13 23:10:01 -0600

    Conectar proyecto con mongodb, e inicialización(creación) de carpetas principales

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }), MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/posts_manager'), PostsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
